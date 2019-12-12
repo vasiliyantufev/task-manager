1e3acdd40216e8795161274e724aed2e5cbc9f5d
Vasiliy Antufev <vasiliyantufev@gmail.com>
2019-12-12 14:54:18 +0300

    prepare

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -76,6 +77,7 @@
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
+
                 </div>
             @endif
 
