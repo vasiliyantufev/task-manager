ac2259c9c75c4967fa72d5c4cf0eabe929d6b7f5
Vasiliy Antufev <vasiliyantufev@gmail.com>
2020-01-31 18:00:28 +0300

    tests fix

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -10,7 +10,7 @@ $factory->define(Task::class, function (Faker $faker) {
     return [
         //
         'title' => $faker->name,
-        'status' => random_int(0, 1),
+        'status_id' => random_int(0, 1),
         'creator_id' => User::all()->random(),
         'executor_id' => User::all()->random(),
     ];
